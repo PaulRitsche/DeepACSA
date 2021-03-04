9111500ccd0cddb6202fe86039e8f64c4b230ab0
Paul Ritsche <71383228+PaulRitsche@users.noreply.github.com>
2021-03-04 15:12:05 +0100

    Added: python package

---
+++ deep_acsauto/__init__.py
@@ -0,0 +1,3 @@
+""" Container for all our package modules."""
+
+__version__ = "0.1.0"
---
+++ deep_acsauto/deep_acsa.py
@@ -0,0 +1,83 @@
+"""Anatomical cross-sectional area evalutaion in Ultrasound images."""
+
+import sys
+import argparse
+
+from predict_muscle_area import calculate_batch, calculate_batch_efov
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description=sys.modules[__name__].__doc__,
+    )
+
+    parser.add_argument(
+        '-rp', '--rootpath',
+        type=str,
+        required=True,
+        default=None,
+        help="path to root directory of images",
+    )
+    parser.add_argument(
+        '-fp', '--flip_flag_path',
+        type=str,
+        default=None,
+        help="file path to txt file containing flipping info",
+    )
+    parser.add_argument(
+        '-mp', '--modelpath',
+        type=str,
+        required=True,
+        default=None,
+        help="file path to .h5 file containing model used for prediction"
+    )
+    parser.add_argument(
+        '-d', "--depth",
+        type=float,
+        required=True,
+        default=4,
+        help="ultrasound scanning depth (cm)"
+    )
+    parser.add_argument(
+        '-sp', "--spacing",
+        type=float,
+        default=5,
+        help="distance (mm) between detetec vertical scaling lines"
+    )
+    parser.add_argument(
+        '-m', "--muscle",
+        type=str,
+        required=True,
+        default="RF",
+        help="muscle that is analyzed",
+    )
+    parser.add_argument(
+        '-s', "--scaling",
+        type=str,
+        required=True,
+        default="EFOV",
+        help="scaling type present in ultrasound image"
+    )
+
+    args = parser.parse_args()
+    print(args)
+    results = []
+
+    if args.scaling == "EFOV":
+        calculate_batch_efov(
+            args.rootpath,
+            args.modelpath,
+            args.depth,
+            args.muscle
+            )
+    else:
+        calculate_batch(
+            args.rootpath,
+            args.flip_flag_path,
+            args.modelpath,
+            args.depth,
+            args.spacing,
+            args.muscle,
+            args.scaling
+            )
