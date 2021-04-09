28464117146ec1e94d35e4e2a5060f8ac8b0f3a3
Paul Ritsche <paul.ritschenibas.ch>
2021-04-09 14:08:26 +0200

    Added: GUI features

--- deep_acsauto/deep_acsa_gui.py
+++ deep_acsauto/deep_acsa_gui.py
@@ -5,52 +5,91 @@ from tkinter import ttk, filedialog
 
 
 
-class FeetToMeters:
+class DeepACSA:
 
     def __init__(self, root):
 
-        root.title("Feet to Meters")
+        root.title("DeepACSA")
 
         mainframe = ttk.Frame(root, padding="10 10 12 12")
         mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
         root.columnconfigure(0, weight=1)
         root.rowconfigure(0, weight=1)
-       	
-       	self.depth = StringVar()
-        depth_entry = ttk.Entry(mainframe, width=7, textvariable=self.depth)
-        depth_entry.grid(column=2, row=1, sticky=(W, E))
+        
+        self.input = StringVar()
+        input_entry = ttk.Entry(mainframe, width=14, textvariable=self.input)
+        input_entry.grid(column=2, row=1, sticky=(W, E))
+
+        self.model = StringVar()
+        model_entry = ttk.Entry(mainframe, width=14, textvariable=self.model)
+        model_entry.grid(column=2, row=2, sticky=(W, E))
+
+        self.flags = StringVar()
+        flags_entry = ttk.Entry(mainframe, width=14, textvariable=self.flags)
+        flags_entry.grid(column=2, row=3, sticky=(W, E))
+
+
+        self.depth = StringVar()
+        depth_entry = ttk.Entry(mainframe, width=7, textvariable=self.depth) #textvariables are kind of globa variables
+        depth_entry.grid(column=2, row=5, sticky=(W, E))
+
         self.spacing = StringVar()
         spacing_entry = ttk.Entry(mainframe, width=7, textvariable=self.spacing)
-        spacing_entry.grid(column=2, row=2, sticky(W, E))
+        spacing_entry.grid(column=2, row=6, sticky=(W, E))
         
+        ## Buttons 
+        # Input directory
+        input_button = ttk.Button(mainframe, text="Input", command=self.get_root_dir)
+        input_button.grid(column=3, row=1, sticky=E)
+        # Model path
+        model_button = ttk.Button(mainframe, text="Model", command=self.get_model_path)
+        model_button.grid(column=3, row=2, sticky=E)
+        # Flip Flag path
+        flags_button = ttk.Button(mainframe, text="Flip Flag", command=self.get_flag_dir)
+        flags_button.grid(column=3, row=3, sticky=E)
+        # Break Button
+        break_button = ttk.Button(mainframe, text="Break")
+        break_button.grid(column=1, row=7, sticky=W)
+        # Run Button
+        run_button = ttk.Button(mainframe, text="Run", command=self.get_root_dir)
+        run_button.grid(column=2, row=7, sticky=(W, E))
 
-        ttk.Label(mainframe, textvariable=self.meters).grid(column=2, row=2, sticky=(W, E))
-        ttk.Button(mainframe, text="Calculate", command=self.calculate).grid(column=3, row=3, sticky=W)
-
-        ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
-        ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
-        ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
+        ttk.Label(mainframe, text="Root Directory").grid(column=1, row=1, sticky=W)
+        ttk.Label(mainframe, text="Model Path").grid(column=1, row=2, sticky=W)
+        ttk.Label(mainframe, text="Flip Flag Path").grid(column=1, row=3, sticky=W)
+        ttk.Label(mainframe, text="Depth (cm)").grid(column=1, row=5, sticky=W)
+        ttk.Label(mainframe, text="Spacing (mm)").grid(column=1, row=6, sticky=W)
 
         for child in mainframe.winfo_children(): 
             child.grid_configure(padx=5, pady=5)
 
-        feet_entry.focus()
-        root.bind("<Return>", self.calculate)
+        depth_entry.focus()
+        root.bind("<Return>", self.get_root_dir) #when return is pressed, same as button
         
-    def calculate(self, *args):
-        try:
-            value = float(self.feet.get())
-            self.meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
-        except ValueError:
-            pass
 
 
-    def get_input_dir(self):
+    def get_root_dir(self):
 
-    	input_dir = filedialog.askdirectory()
-    	return input_dir
+        root_dir = filedialog.askdirectory()
+        self.input.set(root_dir)
+        return root_dir
+
+    def get_model_path(self): 
+
+        model_dir = filedialog.askdirectory()
+        self.model.set(model_dir)
+        return model_dir
+
+    def get_flag_dir(self): 
+
+        flag_dir = filedialog.askdirectory()
+        self.flags.set(flag_dir)
+        return flag_dir
+
+    # def do_break(self): 
+
 
 
 root = Tk()
-FeetToMeters(root)
+DeepACSA(root)
 root.mainloop()
