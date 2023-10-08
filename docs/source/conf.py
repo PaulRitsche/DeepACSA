4c5bc2b4ccfdbc8da34b1a198c8160b521223818
Paul Ritsche <71383228+PaulRitsche@users.noreply.github.com>
2023-10-08 23:05:53 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,8 +21,7 @@ release = "0.3.1"
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ["sphinx.ext.autodoc"]
-templates_path = ["_templates"]
-exclude_patterns = []
+
 
 pygments_style = "sphinx"
 
